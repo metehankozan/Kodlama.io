099681967cdef2bbe1542029acfc91572eb8d472
Metehan Kozan <80221139+metehankozan@users.noreply.github.com>
2023-02-19 23:48:38 +0300

    add mapper and new business cases

--- Java2022/Week 4/kodlama.io.devs/Devs/src/main/java/Kodlama/io/Devs/business/responses/GetAllSubTechnologiesResponse.java
+++ Java2022/Week 4/kodlama.io.devs/Devs/src/main/java/Kodlama/io/Devs/business/responses/GetAllSubTechnologiesResponse.java
@@ -7,7 +7,8 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class GetAllSubLanguagesResponse {
+public class GetAllSubTechnologiesResponse {
     private int id;
     private String name;
+    private String languageName;
 }
