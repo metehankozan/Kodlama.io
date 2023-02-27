819ba8cae7feed11569d9f92ca5d81cf1dafe130
Metehan Kozan <80221139+metehankozan@users.noreply.github.com>
2023-02-28 00:22:15 +0300

    add new entities, request & response patterns, exceptions and some AOP

--- Java2022/campspring/rentACar/src/main/java/kodlama/io/rentACar/entities/concretes/Car.java
+++ Java2022/campspring/rentACar/src/main/java/kodlama/io/rentACar/entities/concretes/Car.java
@@ -6,23 +6,32 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.List;
 
-@Table(name = "brands")
+@Table(name = "cars")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class Brand {
+public class Car {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
 
-    @Column(name = "name")
-    private String name;
+    @Column(name = "plate", unique = true)
+    private String plate;
 
-    @OneToMany(mappedBy = "brand")
-    private List<Model> models;
+    @Column(name = "dailyPrice")
+    private double dailyPrice;
+
+    @Column(name = "modelYear")
+    private int modelYear;
+
+    @Column(name = "state")
+    private int state;
+
+    @ManyToOne
+    @JoinColumn(name = "model_id")
+    private Model model;
 }
