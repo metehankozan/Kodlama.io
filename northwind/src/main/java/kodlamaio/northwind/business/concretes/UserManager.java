105c0d835431e3b546f350a4d50bebe14aebf898
Metehan Kozan <80221139+metehankozan@users.noreply.github.com>
2024-01-19 18:14:45 +0300

    add northwind project

---
+++ northwind/src/main/java/kodlamaio/northwind/business/concretes/UserManager.java
@@ -0,0 +1,29 @@
+package kodlamaio.northwind.business.concretes;
+
+import kodlamaio.northwind.business.abstracts.UserService;
+import kodlamaio.northwind.core.dataAccess.UserDao;
+import kodlamaio.northwind.core.entities.User;
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
+import kodlamaio.northwind.core.utilities.results.SuccessDataResult;
+import kodlamaio.northwind.core.utilities.results.SuccessResult;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserManager implements UserService {
+
+    private UserDao userDao;
+
+    @Override
+    public Result add(User user) {
+        userDao.save(user);
+        return new SuccessResult("Kullanıcı eklendi");
+    }
+
+    @Override
+    public DataResult<User> findByEmail(String email) {
+        return new SuccessDataResult<>(userDao.findByEmail(email), "Kullanıcı bulundu");
+    }
+}
